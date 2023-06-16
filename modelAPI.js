cd7b3da45a51e2a9843c00d38374a5e9ed6a65b3
dyploxaurus <mumutmuti29@gmail.com>
2023-06-16 17:25:18 +0700

    len

--- modelAPI.js
+++ modelAPI.js
@@ -1,20 +1,19 @@
 const axios = require('axios');
 
 function callModelAPI(data) {
-  const apiUrl = 'https://chromamatch.up.railway.app/'; // Ganti dengan URL API model.h5 terpisah
+  const apiUrl = 'https://chromamatch.up.railway.app/';
 
   axios.post(apiUrl, { data }, { headers: { 'Content-Type': 'application/json' } })
     .then(response => {
-      // Tangani hasil respons dari API model.h5 di sini
+
       const result = response.data;
       console.log('Hasil prediksi:', result);
-      // Lakukan tindakan selanjutnya dengan hasil prediksi
     })
     .catch(error => {
       if (error.response) {
         console.error('Error respons:', error.response.data);
       } else if (error.request) {
-        console.error('Tidak ada respons:', error.request);
+        console.error('No respons:', error.request);
       } else {
         console.error('Error:', error.message);
       }
